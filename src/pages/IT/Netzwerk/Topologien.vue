7a6ddd9fe61ea391d82dc28efe916530a0a25355
kurtn3x <kurtn3x@proton.me>
2024-02-01 16:35:26 +0100

    m

--- src/pages/IT/Netzwerk/Topologien.vue
+++ src/pages/IT/Netzwerk/Topologien.vue
@@ -12,91 +12,28 @@
       Netzwerktopologien
     </div>
     <q-separator class="q-mt-md" />
+
     <div class="row justify-center">
-      <table class="tg q-mt-lg">
-        <thead>
-          <tr>
-            <th><a class="text-weight-bold">Skizze</a></th>
-            <th><a class="text-weight-bold">Topologie</a></th>
-            <th><a class="text-weight-bold text-green">Vorteile</a></th>
-            <th><a class="text-weight-bold text-red">Nachteile</a></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="tg-0lax">
-              <q-img
-                class="fit bg-grey-5"
-                :src="src + '/Netzwerke/Topologien/Bustopologie.png'"
-                style="max-width: 200px"
-                @click="
-                  show_img = true;
-                  popupsrc = '/Netzwerke/Topologien/Bustopologie.png';
-                "
-              >
-                <div
-                  class="absolute-bottom-right text-subtitle2"
-                  style="
-                    height: 40px;
-                    font-size: 10px;
-                    background-color: transparent;
-                  "
-                >
-                  Click for full size
-                </div>
-              </q-img>
-            </td>
-            <td class="text-h6 text-weight-bold text-center">Bustopologie</td>
-            <td class="tg-0lax">
-              Geringe Kosten<br /><br />einfache & kurze Verkabelung
-            </td>
-            <td class="tg-0lax">
-              Bei Kabelbruch fällt Netz aus<br /><br />Kollisionsmöglichkeit und
-              ein Sender blockiert alle Anderen <br />Jeder kann Datenverkehr
-              mithören
-            </td>
-          </tr>
-          <tr>
-            <td class="tg-0lax">
-              <q-img
-                class="fit bg-grey-5"
-                :src="src + '/Netzwerke/Topologien/Ringtopologie.png'"
-                style="max-width: 200px"
-                @click="
-                  show_img = true;
-                  popupsrc = '/Netzwerke/Topologien/Ringtopologie.png';
-                "
-              >
-                <div
-                  class="absolute-bottom-right text-subtitle2"
-                  style="
-                    height: 40px;
-                    font-size: 10px;
-                    background-color: transparent;
-                  "
-                >
-                  Click for full size
-                </div>
-              </q-img>
-            </td>
-            <td class="text-h6 text-weight-bold text-center">Ringtopologie</td>
-            <td class="tg-0lax">
-              Zugriffszeit vorhersehbar<br /><br />Keine Datenkollosionen
-            </td>
-            <td class="tg-0lax">
-              höhere Latenzzeiten<br /><br />höherer Verkabelungsaufwand als Bus
-            </td>
-          </tr>
-          <tr>
-            <td class="tg-0lax items">
-              <div class="">
+      <div style="overflow-x: auto">
+        <table class="tg q-mt-lg" style="min-width: 700px">
+          <thead>
+            <tr>
+              <th><a class="text-weight-bold">Skizze</a></th>
+              <th><a class="text-weight-bold">Topologie</a></th>
+              <th><a class="text-weight-bold text-green">Vorteile</a></th>
+              <th><a class="text-weight-bold text-red">Nachteile</a></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="tg-0lax">
                 <q-img
                   class="fit bg-grey-5"
-                  :src="src + '/Netzwerke/Topologien/Sterntopologie.png'"
+                  :src="src + '/Netzwerke/Topologien/Bustopologie.png'"
                   style="max-width: 200px"
                   @click="
                     show_img = true;
-                    popupsrc = '/Netzwerke/Topologien/Sterntopologie.png';
+                    popupsrc = '/Netzwerke/Topologien/Bustopologie.png';
                   "
                 >
                   <div
@@ -110,126 +47,197 @@
                     Click for full size
                   </div>
                 </q-img>
-              </div>
-            </td>
-            <td class="text-h6 text-weight-bold text-center">
-              Sterntopologie<br />(kann zu Baum erweitert werden)<br />
-            </td>
-            <td class="tg-0lax">
-              Einfache Erweiterung<br /><br />Kabelbruch führt nicht zum
-              Netzausfall
-            </td>
-            <td class="tg-0lax">
-              hoher Verkabelungsaufwand<br /><br />Netzausfall bei Ausfall des
-              zentralen Vertreters
-            </td>
-          </tr>
-          <tr>
-            <td class="tg-0lax">
-              <q-img
-                class="fit bg-grey-5"
-                :src="src + '/Netzwerke/Topologien/baum.png'"
-                style="max-width: 200px; min-width: 125px"
-                @click="
-                  show_img = true;
-                  popupsrc = '/Netzwerke/Topologien/baum.png';
-                "
-              >
-                <div
-                  class="absolute-bottom-right text-subtitle2"
-                  style="
-                    height: 40px;
-                    font-size: 10px;
-                    background-color: transparent;
+              </td>
+              <td class="text-h6 text-weight-bold text-center">Bustopologie</td>
+              <td class="tg-0lax">
+                Geringe Kosten<br /><br />einfache & kurze Verkabelung
+              </td>
+              <td class="tg-0lax">
+                Bei Kabelbruch fällt Netz aus<br /><br />Kollisionsmöglichkeit
+                und ein Sender blockiert alle Anderen <br />Jeder kann
+                Datenverkehr mithören
+              </td>
+            </tr>
+            <tr>
+              <td class="tg-0lax">
+                <q-img
+                  class="fit bg-grey-5"
+                  :src="src + '/Netzwerke/Topologien/Ringtopologie.png'"
+                  style="max-width: 200px"
+                  @click="
+                    show_img = true;
+                    popupsrc = '/Netzwerke/Topologien/Ringtopologie.png';
                   "
                 >
-                  Click for full size
+                  <div
+                    class="absolute-bottom-right text-subtitle2"
+                    style="
+                      height: 40px;
+                      font-size: 10px;
+                      background-color: transparent;
+                    "
+                  >
+                    Click for full size
+                  </div>
+                </q-img>
+              </td>
+              <td class="text-h6 text-weight-bold text-center">
+                Ringtopologie
+              </td>
+              <td class="tg-0lax">
+                Zugriffszeit vorhersehbar<br /><br />Keine Datenkollosionen
+              </td>
+              <td class="tg-0lax">
+                höhere Latenzzeiten<br /><br />höherer Verkabelungsaufwand als
+                Bus
+              </td>
+            </tr>
+            <tr>
+              <td class="tg-0lax items">
+                <div class="">
+                  <q-img
+                    class="fit bg-grey-5"
+                    :src="src + '/Netzwerke/Topologien/Sterntopologie.png'"
+                    style="max-width: 200px"
+                    @click="
+                      show_img = true;
+                      popupsrc = '/Netzwerke/Topologien/Sterntopologie.png';
+                    "
+                  >
+                    <div
+                      class="absolute-bottom-right text-subtitle2"
+                      style="
+                        height: 40px;
+                        font-size: 10px;
+                        background-color: transparent;
+                      "
+                    >
+                      Click for full size
+                    </div>
+                  </q-img>
                 </div>
-              </q-img>
-            </td>
-            <td class="text-h6 text-weight-bold text-center">Baumtopologie</td>
-            <td class="tg-0lax">
-              Ausfallsicherheit von Endgeräten<br /><br />Einfach erweiterbar
-            </td>
-            <td class="tg-0lax">
-              Ausfall eines Verteilers sorgt für Ausfall des kompletten
-              Unterbaums<br /><br />
-              Mit zunehmender Tiefe immer höhere Latenzen
-            </td>
-          </tr>
-          <tr>
-            <td class="tg-0lax">
-              <q-img
-                class="fit bg-grey-5"
-                :src="src + '/Netzwerke/Topologien/Maschentopologie.png'"
-                style="max-width: 200px; min-width: 125px"
-                @click="
-                  show_img = true;
-                  popupsrc = '/Netzwerke/Topologien/Maschentopologie.png';
-                "
-              >
-                <div
-                  class="absolute-bottom-right text-subtitle2"
-                  style="
-                    height: 40px;
-                    font-size: 10px;
-                    background-color: transparent;
+              </td>
+              <td class="text-h6 text-weight-bold text-center">
+                Sterntopologie<br />(kann zu Baum erweitert werden)<br />
+              </td>
+              <td class="tg-0lax">
+                Einfache Erweiterung<br /><br />Kabelbruch führt nicht zum
+                Netzausfall
+              </td>
+              <td class="tg-0lax">
+                hoher Verkabelungsaufwand<br /><br />Netzausfall bei Ausfall des
+                zentralen Vertreters
+              </td>
+            </tr>
+            <tr>
+              <td class="tg-0lax">
+                <q-img
+                  class="fit bg-grey-5"
+                  :src="src + '/Netzwerke/Topologien/baum.png'"
+                  style="max-width: 200px; min-width: 125px"
+                  @click="
+                    show_img = true;
+                    popupsrc = '/Netzwerke/Topologien/baum.png';
                   "
                 >
-                  Click for full size
-                </div>
-              </q-img>
-            </td>
-            <td class="text-h6 text-weight-bold text-center">
-              Maschentopologie
-            </td>
-            <td class="tg-0lax">
-              Unendliche Netzausdehnung möglich<br /><br />hohe
-              Ausfallsicherheit
-            </td>
-            <td class="tg-0lax">
-              aufwendige Administration und komplexes Routing<br /><br />teure
-              Vernetzung
-            </td>
-          </tr>
+                  <div
+                    class="absolute-bottom-right text-subtitle2"
+                    style="
+                      height: 40px;
+                      font-size: 10px;
+                      background-color: transparent;
+                    "
+                  >
+                    Click for full size
+                  </div>
+                </q-img>
+              </td>
+              <td class="text-h6 text-weight-bold text-center">
+                Baumtopologie
+              </td>
+              <td class="tg-0lax">
+                Ausfallsicherheit von Endgeräten<br /><br />Einfach erweiterbar
+              </td>
+              <td class="tg-0lax">
+                Ausfall eines Verteilers sorgt für Ausfall des kompletten
+                Unterbaums<br /><br />
+                Mit zunehmender Tiefe immer höhere Latenzen
+              </td>
+            </tr>
+            <tr>
+              <td class="tg-0lax">
+                <q-img
+                  class="fit bg-grey-5"
+                  :src="src + '/Netzwerke/Topologien/Maschentopologie.png'"
+                  style="max-width: 200px; min-width: 125px"
+                  @click="
+                    show_img = true;
+                    popupsrc = '/Netzwerke/Topologien/Maschentopologie.png';
+                  "
+                >
+                  <div
+                    class="absolute-bottom-right text-subtitle2"
+                    style="
+                      height: 40px;
+                      font-size: 10px;
+                      background-color: transparent;
+                    "
+                  >
+                    Click for full size
+                  </div>
+                </q-img>
+              </td>
+              <td class="text-h6 text-weight-bold text-center">
+                Maschentopologie
+              </td>
+              <td class="tg-0lax">
+                Unendliche Netzausdehnung möglich<br /><br />hohe
+                Ausfallsicherheit
+              </td>
+              <td class="tg-0lax">
+                aufwendige Administration und komplexes Routing<br /><br />teure
+                Vernetzung
+              </td>
+            </tr>
 
-          <tr>
-            <td class="tg-0lax">
-              <q-img
-                class="fit bg-grey-5"
-                :src="src + '/Netzwerke/Topologien/zell.png'"
-                style="max-width: 200px; min-width: 125px"
-                @click="
-                  show_img = true;
-                  popupsrc = '/Netzwerke/Topologien/zell.png';
-                "
-              >
-                <div
-                  class="absolute-bottom-right text-subtitle2"
-                  style="
-                    height: 40px;
-                    font-size: 10px;
-                    background-color: transparent;
+            <tr>
+              <td class="tg-0lax">
+                <q-img
+                  class="fit bg-grey-5"
+                  :src="src + '/Netzwerke/Topologien/zell.png'"
+                  style="max-width: 200px; min-width: 125px"
+                  @click="
+                    show_img = true;
+                    popupsrc = '/Netzwerke/Topologien/zell.png';
                   "
                 >
-                  Click for full size
-                </div>
-              </q-img>
-            </td>
-            <td class="text-h6 text-weight-bold text-center">
-              Zelltopologie (Drahtlos)
-            </td>
-            <td class="tg-0lax">
-              Keine Kabel<br /><br />hohe Ausfallsicherheit
-            </td>
-            <td class="tg-0lax">
-              Störanfällig, begrenzte Reichweite <br />
-              <br />
-              Unsicher, jeder kann mithören
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                  <div
+                    class="absolute-bottom-right text-subtitle2"
+                    style="
+                      height: 40px;
+                      font-size: 10px;
+                      background-color: transparent;
+                    "
+                  >
+                    Click for full size
+                  </div>
+                </q-img>
+              </td>
+              <td class="text-h6 text-weight-bold text-center">
+                Zelltopologie (Drahtlos)
+              </td>
+              <td class="tg-0lax">
+                Keine Kabel<br /><br />hohe Ausfallsicherheit
+              </td>
+              <td class="tg-0lax">
+                Störanfällig, begrenzte Reichweite <br />
+                <br />
+                Unsicher, jeder kann mithören
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <q-separator class="q-mt-md" />
     <div
